07c36380e37a2725946ac19e9046e72c16ecfd05
Slavomir Vojacek <slavomirvojacek@Slavomirs-MacBook-Pro.local>
2014-08-13 00:49:24 +0100

    added typekit support + recompiled JS

--- www/assets/js/radio-button.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(".radio-button").click(function(){var t=$(this).attr("data-value"),a=$(this).parent().find("input");return $(this).hasClass("disabled")?!1:(a.val(""),$(this).parent().find(".radio-button").removeClass("selected"),$(this).addClass("selected"),void a.val(t))})});
