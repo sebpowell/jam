f42c58829cfaec1c110395214d8c164ddac7578c
Slavomir Vojacek <slavomir@adbrain.com>
2014-12-02 15:35:10 +0000

    removed grid

--- assets/js/include/grid.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$("body").prepend($('<div id="debug-grid" style="height: ' + $("body").outerHeight() + 'px"></div>'));
-
-	$("body").dblclick(function() {
-		$("#debug-grid").toggleClass("show");
-	});
-});
