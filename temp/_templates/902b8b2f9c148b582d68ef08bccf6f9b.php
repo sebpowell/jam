d7c8c19660ca589dd850e8ba084e59c9b39cf9b2
Sebastien Powell <seb@adbrain.com>
2014-12-20 16:53:14 +0100

    Moved resources to new standalone page

--- temp/_templates/902b8b2f9c148b582d68ef08bccf6f9b.php
+++ temp/_templates/902b8b2f9c148b582d68ef08bccf6f9b.php
@@ -28,24 +28,4 @@
 		<p>In the meantime, <a href="/manifesto">read our story</a>.</p>
 	</div>
 
-	<div class="resources">
-		<h2>Resources</h2>
-		<p class="description">We post tools, articles and resources we use and think you'll find useful too.</p>
-
-		<?php  foreach($template["content"] as $k) {  ?>
-		<article>
-			<p class="title">
-				<a href="<?php echo( $k["link"] ); ?>" target="_blank"><?php echo( $k["title"] ); ?></a>
-				<small><strong><?php echo( $k["date"] ); ?></strong> by <strong><?php echo( $k["author"] ); ?></strong></small>
-			</p>
-			<p class="description"><?php echo( $k["desc"] ); ?></p>
-			<div class="tags flex">
-				<?php  foreach ($k["tags"] as $tag) {  ?>
-				<span class="label <?php echo( $tag ); ?>"><?php echo( $tag ); ?></span>
-				<?php  }  ?>
-			</div>
-		</article>
-		<?php  }  ?>
-
-	</div>
 </article>
