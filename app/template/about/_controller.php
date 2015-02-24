48c5c4699b5f1ff6a71d5ceb8a4393b71ae9fc83
Slavomir Vojacek <slavomir@adbrain.com>
2015-02-24 15:09:35 +0000

    alter team rendering

--- app/template/about/_controller.php
+++ app/template/about/_controller.php
@@ -13,6 +13,38 @@ final class controller extends Controller\baseController
 
 		$this->template->page["title"] = "Manifesto";
 		$this->template->page["act"] = 1;
+
+		$this->template->theTeam = $this->renderTeamMembers();
+	}
+
+	private function renderTeamMembers() {
+		$team = [
+			[
+				"name" => "Seb Powell",
+				"photo" => "seb-powell.png",
+				"desc" => "Head of Design. Amateur developer.",
+				"linkedin" => "sebastienpowell",
+				"twitter" => "sebpowell"
+			],
+			[
+				"name" => "Slavo Vojacek",
+				"photo" => "slavo-vojacek.png",
+				"desc" => "UX Designer &amp; Developer. Lover of fine English literature.",
+				"linkedin" => "slavomirvojacek",
+				"twitter" => "slavomirvojacek"
+			],
+			[
+				"name" => "Mathilde Leo",
+				"photo" => "mathilde-leo.png",
+				"desc" => "Product Owner. Martial arts aficionado.",
+				"linkedin" => "mathildeleo",
+				"twitter" => "Mathilde_Leo"
+			]
+		];
+
+		shuffle($team);
+
+		return $team;
 	}
 
 }
--- temp/_templates/d54099f52bb8c01f4136cdf00d21ad27.php
+++ temp/_templates/d54099f52bb8c01f4136cdf00d21ad27.php
@@ -5,6 +5,7 @@
 
 	<div class="content">
 		<h2>Manifesto</h2>
+
 		<p>Building products is hard. When the three of us started working
 			together at <a href="http://www.adbrain.com">Adbrain</a> a year ago, we struggled. We’d
 			worked on products before, but nothing quite like this (ad tech
@@ -13,51 +14,34 @@
 			What tools are best for the job at hand? How does a good product
 			become great?</p>
 
-			<p>We often wondered how other companies went about building products
+		<p>We often wondered how other companies went about building products
 			from scratch. We realised what we really needed was a place where we
 			could learn from others that have been there and done it. Somewhere
 			where we could discover and understand how all the parts fit
 			together.</p>
 
-			<p>So we decided to start JAM. This is the community we wish we’d had
+		<p>So we decided to start JAM. This is the community we wish we’d had
 			all along, a place where people, across all disciplines can discuss
 			their successes and failures, experiments, tools and techniques, and
 			anything else that gets great products built.</p>
 
-			<p>We hope you’ll join us.</p>
-	<h2>The Team</h2>
-	<div class="flex-row margin-bottom double-unit-layout">
-		<div class="columns-4">
-			<img src="../../assets/images/team/mathilde-leo.png">
-		</div>
+		<p>We hope you’ll join us.</p>
+
+		<h2>The Team</h2>
+
+		<?php  foreach($template["theTeam"] as $k => $v) {  ?>
+		<div class="flex-row margin-bottom double-unit-layout">
+			<div class="columns-4">
+				<img src="/assets/images/team/<?php echo( $v["photo"] ); ?>">
+			</div>
 			<div class="columns-8">
-				<h4>Mathilde Leo</h4>
-				<p>Product Owner. Martial arts aficionado.</p>
-				<a target="_blank" href="http://uk.linkedin.com/in/mathildeleo">LinkedIn</a>
-				<a target="_blank" href="https://twitter.com/Mathilde_Leo">Twitter</a>
+				<h4><?php echo( $v["name"] ); ?></h4>
+
+				<p><?php echo( $v["desc"] ); ?></p>
+				<a target="_blank" href="http://uk.linkedin.com/in/<?php echo( $v["linkedin"] ); ?>">LinkedIn</a>
+				<a target="_blank" href="https://twitter.com/<?php echo( $v["twitter"] ); ?>">Twitter</a>
+			</div>
 		</div>
-	</div>
-	<div class="flex-row margin-bottom double-unit-layout">
-		<div class="columns-4">
-			<img src="../../assets/images/team/seb-powell.png">
-		</div>
-			<div class="columns-8">
-				<h4>Seb Powell</h4>
-				<p>Head of Design. Amateur developer.</p>
-				<a target="_blank" href="http://uk.linkedin.com/in/sebastienpowell">LinkedIn</a>
-				<a target="_blank" href="https://twitter.com/sebpowell">Twitter</a>
-		</div>
-	</div>
-	<div class="flex-row">
-		<div class="columns-4">
-			<img src="../../assets/images/team/slavo-vojacek.png">
-		</div>
-			<div class="columns-8">
-				<h4>Slavo Vojacek</h4>
-				<p>UX Designer &amp; Developer. Lover of fine English literature.</p>
-				<a target="_blank" href="http://uk.linkedin.com/pub/slavo-vojacek/14/6a/78b">LinkedIn</a>
-				<a target="_blank" href="https://twitter.com/slavomirvojacek">Twitter</a>
-		</div>
-	</div>
+		<?php  }  ?>
 	</div>
 </article>
