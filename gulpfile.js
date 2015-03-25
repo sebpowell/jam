6ed87ee1d53286ac1ba35aaef4b2847b4d797935
Slavomir Vojacek <slavomir@adbrain.com>
2015-03-25 16:42:09 +0000

    infrastructure updates

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,6 @@
  */
 var gulp =
 	require("gulp"),
-	sass = require("gulp-ruby-sass"),
-	autoprefixer = require("gulp-autoprefixer"),
-	uglify = require("gulp-uglify"),
-	concat = require("gulp-concat"),
-	notify = require("gulp-notify"),
 	connect = require("gulp-connect-php");
 
 /*
@@ -19,40 +14,4 @@ gulp.task("start-php", function() {
 	});
 });
 
-gulp.task("sass", function() {
-
-	gulp.src("assets/css/include/screen.sass")
-		.pipe(sass({
-			loadPath: process.cwd() + "/assets/css/include",
-			style: "nested"
-		}))
-		.pipe(autoprefixer("last 2 version", "> 1%"))
-		.pipe(gulp.dest("assets/css"))
-		.pipe(notify("SASS successfully compiled!"));
-
-});
-
-gulp.task("uglify", function() {
-
-	gulp.src("assets/js/include/*.js")
-		.pipe(concat("app.js"))
-		.pipe(uglify("app.js"))
-		.pipe(gulp.dest("assets/js"))
-		.pipe(notify("JavaScript successfully compiled!"));
-
-});
-
-gulp.task("watch", function() {
-
-	gulp.watch("assets/css/include/**/*.sass", ["sass"]);
-	gulp.watch("assets/js/include/*.js", ["uglify"]);
-
-});
-
-
-/*
- * Default Gulp task
- */
-gulp.task("default", ["start-php"], function() {
-	gulp.start("watch");
-});
+gulp.task("default", ["start-php"]);
